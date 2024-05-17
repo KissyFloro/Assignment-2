8026605c28f3dc2180744f427f64c8db8d7102c5
Kissy Eve Amor Floro <167738202+KissyFloro@users.noreply.github.com>
2024-05-17 20:45:40 +0800

    Update linalginv.py

--- linalginv.py
+++ linalginv.py
@@ -2,16 +2,16 @@
 
 
 # Example 1:
-# import numpy as np
-# x = np.array([[4,11], [3,2]])
-# result = np.linalg.inv(x)
-# print(result)
+import numpy as np
+x = np.array([[4,11], [3,2]])
+result = np.linalg.inv(x)
+print(result)
 
 # Example 2:
-# import numpy as np
-# x = np.array([[2,7], [1,4]])
-# result = np.linalg.inv(x)
-# print(result)
+import numpy as np
+x = np.array([[2,7], [1,4]])
+result = np.linalg.inv(x)
+print(result)
 
 # Example 3:
 import numpy as np
